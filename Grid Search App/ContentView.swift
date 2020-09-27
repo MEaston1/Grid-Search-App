7b965cb8d20992ac1276754fa7398af23c9b8d56
matthew easton <MatthewEaston@mail.com>
2020-09-27 11:34:49 +0100

    Initial Commit

---
+++ Grid Search App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grid Search App
+//
+//  Created by Matthew Easton on 27/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Grid Search App/Grid_Search_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_Search_AppApp.swift
+//  Grid Search App
+//
+//  Created by Matthew Easton on 27/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Grid_Search_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
