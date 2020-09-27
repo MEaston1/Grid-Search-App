d73321fb0772b37b6fc288afae1bf3dc3f42eff4
matthew easton <MatthewEaston@mail.com>
2020-09-27 12:37:16 +0100

    Grid created

--- Grid Search App/ContentView.swift
+++ Grid Search App/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+class GridViewModel : ObservableObject {
+    @Published var items = 0..<10
+    
+    init() {
+        
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var vm = GridViewModel()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: [
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200))
+                ],spacing: 16, content: {
+                    ForEach(vm.items, id: \.self){
+                        num in
+                        VStack(alignment: .leading){
+                            Spacer()
+                                .frame(width: 100, height: 100)
+                                .background(Color.blue)
+                            Text("App title")
+                                .font(.system(size: 9, weight: .semibold))
+                            Text("Release Date")
+                                .font(.system(size: 9, weight: .regular))
+                            Text("Copyright")
+                                .font(.system(size: 9, weight: .regular))
+                                .foregroundColor(.gray)
+                        }
+                        .padding()
+                        .background(Color.red)
+                    }
+                    
+                    
+                }).padding(.horizontal, 12)
+            }.navigationTitle("Grid Search App")
+        }
     }
 }
 
